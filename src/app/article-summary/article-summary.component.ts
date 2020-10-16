7675d115f71f77278d0bcecffa96dcf88ca467ee
sybille <sybille@pxlwidgets-heroes.com>
2020-10-16 14:47:31 +0200

    :sparkles: Summary component is receiving data

--- src/app/article-summary/article-summary.component.ts
+++ src/app/article-summary/article-summary.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-article-summary',
@@ -6,6 +6,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./article-summary.component.sass']
 })
 export class ArticleSummaryComponent implements OnInit {
+
+  @Input() title: string
+  @Input() author: string
+  @Input() publishedAt: string
+  @Input() description: string
 
   constructor() { }
 

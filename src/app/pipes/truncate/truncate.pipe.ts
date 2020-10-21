53f83cb98a9c777e661c5c402df0a6d2896b3495
sybille <sybille@pxlwidgets-heroes.com>
2020-10-21 13:53:48 +0200

    :sparkles: Truncate pipe

---
+++ src/app/pipes/truncate/truncate.pipe.ts
@@ -0,0 +1,15 @@
+import { Pipe} from '@angular/core';
+
+@Pipe({
+  name: 'limitTo'
+})
+export class TruncatePipe {
+  transform(value: string, args: number) : string {
+    // let limit = args.length > 0 ? parseInt(args[0], 10) : 10;
+    // let trail = args.length > 1 ? args[1] : '...';
+    let limit = args ? args : 10;
+    let trail = '...';
+
+    return value.length > limit ? value.substring(0, limit) + trail : value;
+  }
+}

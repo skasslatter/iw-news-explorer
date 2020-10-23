8c5113040f3cd8a02684aae2e75514a5d0919cd5
sybille <sybille@pxlwidgets-heroes.com>
2020-10-23 10:44:49 +0200

    :white_check_mark: Added tests for truncate pipe

--- src/app/pipes/truncate/truncate.pipe.spec.ts
+++ src/app/pipes/truncate/truncate.pipe.spec.ts
@@ -1,8 +1,15 @@
 import { TruncatePipe } from './truncate.pipe';
 
 describe('TruncatePipe', () => {
+  const pipe = new TruncatePipe();
+
   it('create an instance', () => {
-    const pipe = new TruncatePipe();
     expect(pipe).toBeTruthy();
+  })
+  it('transforms "This is a very long text" to "This ..."', () => {
+    expect(pipe.transform('This is a very long text', 5)).toBe('This ...');
+  });
+  it('transforms "This is a very long text" to "This ..."', () => {
+    expect(pipe.transform('This is a very long text', 50)).toBe('This is a very long text');
   });
 });

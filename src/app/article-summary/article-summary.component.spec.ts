aaa6f47ecfe709bd47102a9789e89f6f3c435eec
sybille <sybille@pxlwidgets-heroes.com>
2020-10-23 14:44:58 +0200

    :white_check_mark: Adding tests

--- src/app/article-summary/article-summary.component.spec.ts
+++ src/app/article-summary/article-summary.component.spec.ts
@@ -18,6 +18,27 @@ import { Button } from "primeng/button";
 describe('ArticleSummaryComponent', () => {
   let component: ArticleSummaryComponent;
   let fixture: ComponentFixture<ArticleSummaryComponent>;
+  const article = {
+    author: "John Doe",
+    description: "Second Trump-Biden debate has fewer interruptions but more counterpunches. " +
+      "Second Trump-Biden debate has fewer interruptions but more counterpunches." +
+      "Second Trump-Biden debate has fewer interruptions but more counterpunches." +
+      "Second Trump-Biden debate has fewer interruptions but more counterpunches." +
+      "Second Trump-Biden debate has fewer interruptions but more counterpunches." +
+      "Second Trump-Biden debate has fewer interruptions but more counterpunches.",
+    publishedAt: "2020-10-23T08:26:58Z",
+    source: {
+      id: "1",
+      name: "Washington Post",
+    },
+    title: "Second Presidential Debate",
+    url: "www.washingtonpost.com",
+    urlToImage: "https://chuckanddons.com/media/wysiwyg/kitten_blog.jpg",
+    content: "NASHVILLE — President Trump tried to cast Joe Biden as a ­scandal-plagued politician who had failed over " +
+      "decades in office, and Biden sought to portray Trump as a demagogue who criminally abused immigrants and " +
+      "mishandled the Coronavirus pandemic as the two presidential contenders counterpunched on a wide range of policy " +
+      "issues in their second and final debate.",
+  }
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
@@ -42,29 +63,9 @@ describe('ArticleSummaryComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(ArticleSummaryComponent);
     component = fixture.componentInstance;
-    component.article = {
-      author: "John Doe",
-      description: "Second Trump-Biden debate has fewer interruptions but more counterpunches. " +
-        "Second Trump-Biden debate has fewer interruptions but more counterpunches." +
-        "Second Trump-Biden debate has fewer interruptions but more counterpunches." +
-        "Second Trump-Biden debate has fewer interruptions but more counterpunches." +
-        "Second Trump-Biden debate has fewer interruptions but more counterpunches." +
-        "Second Trump-Biden debate has fewer interruptions but more counterpunches.",
-      publishedAt: "2020-10-23T08:26:58Z",
-      source: {
-        id: "1",
-        name: "Washington Post",
-      },
-      title: "Second Presidential Debate",
-      url: "www.washingtonpost.com",
-      urlToImage: "https://chuckanddons.com/media/wysiwyg/kitten_blog.jpg",
-      content: "NASHVILLE — President Trump tried to cast Joe Biden as a ­scandal-plagued politician who had failed over " +
-        "decades in office, and Biden sought to portray Trump as a demagogue who criminally abused immigrants and " +
-        "mishandled the Coronavirus pandemic as the two presidential contenders counterpunched on a wide range of policy " +
-        "issues in their second and final debate.",
-    }
+    component.article = article;
     fixture.detectChanges();
-  });
+  })
 
   it('should create', () => {
     expect(component).toBeTruthy();
@@ -72,7 +73,7 @@ describe('ArticleSummaryComponent', () => {
   it('should display the article title', () => {
     const bannerElement: HTMLElement = fixture.nativeElement;
     const title = bannerElement.querySelector('mat-card-title');
-    expect(title.innerHTML).toEqual('Second Presidential Debate');
+    expect(title.innerHTML).toEqual(article.title);
   });
   it('should display the article date', () => {
     const bannerElement: HTMLElement = fixture.nativeElement;
@@ -82,11 +83,12 @@ describe('ArticleSummaryComponent', () => {
   it('should display the article author and source', () => {
     const bannerElement: HTMLElement = fixture.nativeElement;
     const author = bannerElement.querySelectorAll('mat-card-subtitle small')[1];
-    expect(author.innerHTML).toEqual('by John Doe for Washington Post');
+    expect(author.innerHTML).toEqual(`by ${article.author} for ${article.source.name}`);
   });
   it('should display the description limited to 200 characters', () => {
     const bannerElement: HTMLElement = fixture.nativeElement;
     const article = bannerElement.querySelector('mat-card-content p');
     expect(article.innerHTML).toEqual('Second Trump-Biden debate has fewer interruptions but more counterpunches. Second Trump-Biden debate has fewer interruptions but more counterpunches.Second Trump-Biden debate has fewer interruptions b...');
   });
-});
+})
+;
